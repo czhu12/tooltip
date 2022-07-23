92999674685a61cb407d0e73ed1883bf418d1715
Celina <marisa.celina.lopez4@gmail.com>
2022-07-23 13:48:51 -0700

    annotate gem

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,29 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :string
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  github_username        :string
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :string
+#  personal_website       :string
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  sign_in_count          :integer          default(0), not null
+#  twitter_username       :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
